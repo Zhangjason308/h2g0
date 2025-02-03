316f500fd0fd41195ef8c91ad6003bdf28c8f78f
Marwan Zeid <45441599+Sgt-Roukoz@users.noreply.github.com>
2025-02-03 15:08:25 -0500

    Adding map_widget file

---
+++ lib/map_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MapWidget extends StatefulWidget {
+  @override
+  State<MapWidget> createState() => _MapWidget();
+}
+
+class _MapWidget extends State<MapWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
