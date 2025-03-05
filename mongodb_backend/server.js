a597ce2f0c019f8c0e1d1f20a25dc97156c89932
calebluiyee <calebluiyee@cmail.carleton.ca>
2025-03-05 17:10:39 -0500

    a bug fix

--- mongodb_backend/server.js
+++ mongodb_backend/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import { MongoClient } from 'mongodb';
+import pkg from 'mongodb';
+const { MongoClient } = pkg;
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
