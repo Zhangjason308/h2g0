ae6a5f4b0e520473d74689783736a51ee225b787
Triguy12 <tritoncrowley@gmail.com>
2025-03-05 13:50:45 -0500

    Added the connection to the database from MongoDb using node.js, then loaded variables with that information into the MapWidget widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'map_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env"); // First Load dotenv  in main function
@@ -31,10 +33,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 72, 140, 230)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 72, 140, 230)),
         useMaterial3: true,
       ),
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -42,10 +45,56 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
+  Future<Map<String, dynamic>> fetchData() async {
+    //final String? apiUrl = dotenv.env['API_URL'];
+
+    try {
+      //final response = await http.get(Uri.parse('$apiUrl/locations'));
+      final response =
+          await http.get(Uri.parse('http://localhost:5000/locations'));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return data;
+      } else {
+        throw Exception('Failed to load data - 1');
+      }
+    } catch (error) {
+      print('Error fetching data: $error');
+      throw Exception('Failed to load data - 2');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final String? apikey = dotenv.env['PLACES_API_KEY'];
-    
-    return MapWidget(placesAPIKey: apikey);
+
+    //return MapWidget(placesAPIKey: apikey);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Washroom Map")),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: fetchData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final washroomLocations = snapshot.data!['washroomLocations'];
+            final waterFountainLocations = snapshot.data!['fountainLocations'];
+
+            // Return the MapWidget with fetched data
+            return MapWidget(
+              placesAPIKey: apikey, // Pass the API key to MapWidget
+              washroomLocations: washroomLocations,
+              waterFountainLocations: waterFountainLocations,
+            );
+          } else {
+            // If no data available, show a fallback message
+            return Center(child: Text('No data available'));
+          }
+        },
+      ),
+    );
   }
 }
--- lib/map_widget.dart
+++ lib/map_widget.dart
@@ -9,11 +9,16 @@ import 'package:material_floating_search_bar_2/material_floating_search_bar_2.da
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 class MapWidget extends StatefulWidget {
   final String? placesAPIKey;
-  
-  const MapWidget({super.key, required this.placesAPIKey});
+  final List washroomLocations;
+  final List waterFountainLocations;
+
+  const MapWidget(
+      {super.key,
+      required this.placesAPIKey,
+      required this.washroomLocations,
+      required this.waterFountainLocations});
 
   @override
   State<MapWidget> createState() => _MapWidget();
@@ -21,16 +26,19 @@ class MapWidget extends StatefulWidget {
 
 Widget buildMap(AnimatedMapController mapcontroller, BuildContext context) {
   final theme = Theme.of(context);
-  
+
   return FlutterMap(
     mapController: mapcontroller.mapController,
     options: MapOptions(
-      initialCenter: LatLng(45.424721, -75.695000), // Center the map over Ottawa
+      initialCenter:
+          LatLng(45.424721, -75.695000), // Center the map over Ottawa
       initialZoom: 14,
     ),
     children: [
-      TileLayer( // Bring your own tiles
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
+      TileLayer(
+        // Bring your own tiles
+        urlTemplate:
+            'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
         userAgentPackageName: 'com.h2g0.app', // Add your app identifier
         tileProvider: CancellableNetworkTileProvider(),
         // And many more recommended properties!
@@ -40,49 +48,47 @@ Widget buildMap(AnimatedMapController mapcontroller, BuildContext context) {
         alignDirectionOnUpdate: AlignOnUpdate.never,
       ),
       Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: FloatingActionButton(
-                backgroundColor: theme.colorScheme.primary,
-                onPressed: () {
-                  mapcontroller.centerOnPoint(
-                    LatLng(45.424721, -75.695000), zoom: 16
-                  );
-                },
-                child: Icon(
-                  Icons.my_location,
-                  color: theme.colorScheme.onPrimary,
-                ),
+          alignment: Alignment.bottomRight,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: FloatingActionButton(
+              backgroundColor: theme.colorScheme.primary,
+              onPressed: () {
+                mapcontroller.centerOnPoint(LatLng(45.424721, -75.695000),
+                    zoom: 16);
+              },
+              child: Icon(
+                Icons.my_location,
+                color: theme.colorScheme.onPrimary,
               ),
-            )
-      ),
+            ),
+          )),
       RichAttributionWidget(
-        alignment: AttributionAlignment.bottomLeft, // Include a stylish prebuilt attribution widget that meets all requirments
+        alignment: AttributionAlignment
+            .bottomLeft, // Include a stylish prebuilt attribution widget that meets all requirments
         attributions: [
           TextSourceAttribution(
             'OpenStreetMap contributors',
-            onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // (external)
+            onTap: () => launchUrl(
+                Uri.parse('https://openstreetmap.org/copyright')), // (external)
           ),
           // Also add images...
         ],
       ),
     ],
-        );
+  );
 }
 
-
-class _MapWidget extends State<MapWidget> with TickerProviderStateMixin{
-  
+class _MapWidget extends State<MapWidget> with TickerProviderStateMixin {
   final _controller = FloatingSearchBarController();
   List<Place> placesList = [];
 
   late final _animatedMapController = AnimatedMapController(
-      vsync: this,
-      duration: const Duration(milliseconds: 2000),
-      curve: Curves.easeInOutSine,
-      cancelPreviousAnimations: true, // Default to false
-      );
+    vsync: this,
+    duration: const Duration(milliseconds: 2000),
+    curve: Curves.easeInOutSine,
+    cancelPreviousAnimations: true, // Default to false
+  );
 
   @override
   void dispose() {
@@ -92,20 +98,21 @@ class _MapWidget extends State<MapWidget> with TickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
-    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
     String selectedID;
     String? apiKey = widget.placesAPIKey;
 
     void getResults(String input) async {
-
-      String baseURL='https://maps.googleapis.com/maps/api/place/autocomplete/json';
+      String baseURL =
+          'https://maps.googleapis.com/maps/api/place/autocomplete/json';
       String type = 'geocode';
       String location = '45.424721 -75.695000';
       String radius = '5000';
       //TODO Add session token
 
-      
-      String request = '$baseURL?input=$input&key=$apiKey&type=$type&location=$location&radius=$radius';
+      String request =
+          '$baseURL?input=$input&key=$apiKey&type=$type&location=$location&radius=$radius';
       Response response = await Dio().get(request);
 
       final predictions = response.data['predictions'];
@@ -126,103 +133,102 @@ class _MapWidget extends State<MapWidget> with TickerProviderStateMixin{
     }
 
     void getLatLng(String placeId, String address) async {
-      String baseURL='https://maps.googleapis.com/maps/api/place/details/json';
+      String baseURL =
+          'https://maps.googleapis.com/maps/api/place/details/json';
       String fields = 'geometry';
 
-      String request = '$baseURL?input=$address&placeid=$placeId&fields=$fields&key=$apiKey';
+      String request =
+          '$baseURL?input=$address&placeid=$placeId&fields=$fields&key=$apiKey';
       Response response = await Dio().get(request);
 
       final result = response.data['result']['geometry']['location'];
 
       double lat = result['lat'] as double;
       double lng = result['lng'] as double;
-      
+
       _animatedMapController.centerOnPoint(LatLng(lat, lng), zoom: 16);
     }
 
     return Scaffold(
-      body: Stack(
-        children: [
-          buildMap(_animatedMapController, context),
-          FloatingSearchBar(
-            controller: _controller,
-            hint: 'Search...',
-            scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-            transitionDuration: const Duration(milliseconds: 800),
-            transitionCurve: Curves.easeInOut,
-            physics: const BouncingScrollPhysics(),
-            axisAlignment: isPortrait ? 0.0 : -1.0,
-            openAxisAlignment: 0.0,
-            width: isPortrait ? 600 : 500,
-            textInputAction: TextInputAction.next,
-            clearQueryOnClose: false,
-            onFocusChanged: (isFocused) => {
-              if (!isFocused) {
+      body: Stack(children: [
+        buildMap(_animatedMapController, context),
+        FloatingSearchBar(
+          controller: _controller,
+          hint: 'Search...',
+          scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+          transitionDuration: const Duration(milliseconds: 800),
+          transitionCurve: Curves.easeInOut,
+          physics: const BouncingScrollPhysics(),
+          axisAlignment: isPortrait ? 0.0 : -1.0,
+          openAxisAlignment: 0.0,
+          width: isPortrait ? 600 : 500,
+          textInputAction: TextInputAction.next,
+          clearQueryOnClose: false,
+          onFocusChanged: (isFocused) => {
+            if (!isFocused)
+              {
                 setState(() {
                   placesList = [];
                 })
               }
-            },
-            debounceDelay: const Duration(milliseconds: 500),
-            onSubmitted: (query) {
-              placesList = [];
-              _controller.close();
-            },
-            onQueryChanged: (query) {
-              if (query != '') {
-                getResults(query);
-              } else {
-                setState(() {
-                  placesList = [];
-                });
-              }
-            },
-            // Specify a custom transition to be used for
-            // animating between opened and closed stated.
-            transition: CircularFloatingSearchBarTransition(),
-            actions: [
-              FloatingSearchBarAction(
-                showIfOpened: false,
-                child: CircularButton(
-                  icon: const Icon(Icons.place),
-                  onPressed: () {},
-                ),
+          },
+          debounceDelay: const Duration(milliseconds: 500),
+          onSubmitted: (query) {
+            placesList = [];
+            _controller.close();
+          },
+          onQueryChanged: (query) {
+            if (query != '') {
+              getResults(query);
+            } else {
+              setState(() {
+                placesList = [];
+              });
+            }
+          },
+          // Specify a custom transition to be used for
+          // animating between opened and closed stated.
+          transition: CircularFloatingSearchBarTransition(),
+          actions: [
+            FloatingSearchBarAction(
+              showIfOpened: false,
+              child: CircularButton(
+                icon: const Icon(Icons.place),
+                onPressed: () {},
               ),
-              FloatingSearchBarAction.searchToClear(
-                showIfClosed: false,
-              ),
-            ],
-            builder: (context, transition) {
-              if (placesList.isEmpty) {
-                return Container();
-              }
-              else {
-                return ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Material(
-                    color: Colors.white,
-                    elevation: 4.0,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: placesList.map((place) {
-                        return ListTile(
+            ),
+            FloatingSearchBarAction.searchToClear(
+              showIfClosed: false,
+            ),
+          ],
+          builder: (context, transition) {
+            if (placesList.isEmpty) {
+              return Container();
+            } else {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Material(
+                  color: Colors.white,
+                  elevation: 4.0,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: placesList.map((place) {
+                      return ListTile(
                           title: Text(place.address),
                           onTap: () {
                             selectedID = place.placeid;
                             _controller.query = place.address;
                             _controller.close();
                             getLatLng(selectedID, place.address);
-                          }
-                        );
-                      }).toList(),
-                    ),
+                          });
+                    }).toList(),
                   ),
-                );
-              }
-            },
-          )
-        ]
-      ),
-      );
+                ),
+              );
+            }
+          },
+        )
+      ]),
+    );
   }
 }
